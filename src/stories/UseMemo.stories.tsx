02671e4a72297818d7f2ed24d1de78c416f2badd
Pavel Morozuk <morozuki@mail.ru>
2022-06-20 00:08:31 +0300

    UseMemo 18

---
+++ src/stories/UseMemo.stories.tsx
@@ -0,0 +1,45 @@
+import {useMemo, useState} from "react";
+
+export default {
+    title: 'useMemo'
+}
+
+
+export const DifficultCountingExample = () => {
+    const [a, setA] = useState<number>(0)
+    const [b, setB] = useState<number>(0)
+
+    let resultA: number
+    let resultB = 1
+
+    resultA = useMemo(()=>{
+        let tempResultA = 1
+        for (let i = 1; i <= a; i++) {
+            let fake = 0
+            while (fake<100000000){
+                fake++
+                const fakeValue = Math.random()
+            }
+            tempResultA *=i
+        }
+        return tempResultA
+    }, [a])
+
+
+    for (let i = 1; i <= b; i++) {
+        resultB *=i
+    }
+
+    return <>
+        <input value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/>
+        <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
+        <hr/>
+        <div>
+            Result for a: {resultA}
+        </div>
+        <div>
+            Result for b: {resultB}
+        </div>
+    </>
+}
+
